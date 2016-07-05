76eaae57b78fb1ef3f32395d3e83220c36df20ac
jacklametta <alessio.nazio@gmail.com>
2016-07-05 17:06:53 +0200

    Prova Commit

--- src/byob_v1/Byob_v1.java
+++ src/byob_v1/Byob_v1.java
@@ -15,7 +15,7 @@ public class Byob_v1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ciao, Domè. Te vedo chiù leoness\', chiù selvagg\'!");
     }
     
 }
