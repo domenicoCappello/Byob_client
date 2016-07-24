2531f58e225c69d2645936661c022f7f3fee7b1c
jacklametta <alessio.nazio@gmail.com>
2016-07-24 15:46:10 +0200

    idGeneration & hashFunction functions added.

--- src/byob_v1/URLDetails.java
+++ src/byob_v1/URLDetails.java
@@ -5,6 +5,10 @@
  */
 package byob_v1;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Calendar;
@@ -15,6 +19,7 @@ import java.util.Calendar;
  */
 public class URLDetails {
     
+    String idBot;
     private String URL;
     private int minWaitTime;
     private int maxWaitTime;
@@ -78,6 +83,68 @@ public class URLDetails {
                (am   && sleepAMHours() )||
                (!am  && sleepPMHours() );
     }
+    
+    /**
+     * Th function returns a unique bot id using a hash of the host hardware
+     * information. 
+     * @return unique ID of the bot
+     */
+    public String idGeneration() {
+        String hardware = "";
+        String command = "";
+        switch(Tools.getOs().toUpperCase()){
+            case "LINUX":
+                command = "lshw | grep -e serial -e product | grep -v Controller | grep -v None";
+                break;
+                
+            case "OSX":
+                // TO-DO
+                break;
+                
+            case "WINDOWS":
+                // TO-DO
+                break;
+        }
+        
+        hardware = Tools.runCmd(command);
+        return hashFunction(hardware);
+    }
+    
+    /**
+     * The function returns the MD5 Checksum of a string.
+     * @param hw the string youwant to compute the checksum of
+     * @returnthe checksum of the string
+     */
+    public static String hashFunction(String hw) {
+        
+        byte[] bytesMsg = null;
+        byte[] bytesDigest = null;
+        MessageDigest md = null;
+        String hashText = "";
+        
+        try {
+            bytesMsg = hw.getBytes("UTF-8");
+        }
+        catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            md = MessageDigest.getInstance("MD5");
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        bytesDigest = md.digest(bytesMsg);
+        hashText = (new BigInteger(1, bytesDigest)).toString(16);
+        while(hashText.length() < 32) {
+            hashText = "0"+hashText;
+        }
+        
+        return hashText;
+    }
+    
     
     private boolean sleepOddDays(){
         return sleepMode.toLowerCase().contains("o");
