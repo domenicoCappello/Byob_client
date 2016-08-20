a848bf97779cf8ef702c665fbbdf5ad74b7e4a75
jacklametta <alessio.nazio@gmail.com>
2016-08-20 17:49:54 +0200

    validateIpAddress Added

--- src/byob_v1/Parser.java
+++ src/byob_v1/Parser.java
@@ -170,4 +170,15 @@ public class Parser {
         return detail;
     }
     
+     public boolean validateIPAddress( String ipAddress ) { 
+        String[] tokens = ipAddress.split("\\."); 
+        if (tokens.length != 4) 
+             return false; 
+        for (String str : tokens) { 
+            int i = Integer.parseInt(str); 
+            if ((i < 0) || (i > 255)) 
+                return false;
+        }
+            return true; 
+     }
 }
